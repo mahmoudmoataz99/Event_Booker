7d73e535657159b2779017a3f91ab95dfab69cec
mahmoudmoataz99 <mahmoudmoataz99@gmail.com>
2025-06-24 10:13:51 +0300

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react(),tailwindcss()],
 })
